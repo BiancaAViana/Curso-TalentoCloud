5c5068383b9b2910a0c51d4a0a566d8fee808c2c
Bianca Viana <bianca.albuquerquevn@gmail.com>
2023-09-26 21:11:45 -0300

    Fazendo um merge

--- testando.js
+++ testando.js
@@ -1,3 +1,5 @@
 console.log('Isto é um teste de branch')
 
-console.log('Isso irá para a minha nova branch. Finja que é uma funcionalidade.')
+console.log('Isso irá para a minha nova branch. Finja que é uma funcionalidade. Isso vem antes do merge')
+
+console.log('Isso irá para a minha nova branch. Finja que é uma funcionalidade. Isso vem depois do merge.')
