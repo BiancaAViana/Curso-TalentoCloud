e69c3e9c867c1f6b57f312e5928c96feebb51078
Bianca Viana <bianca.albuquerquevn@gmail.com>
2023-09-26 21:09:14 -0300

    a

--- testando.js
+++ testando.js
@@ -1,3 +1,3 @@
 console.log('Isto é um teste de branch')
 
-console.log('Isso irá para a minha nova branch. Finja que é uam funcionalidade.')
+console.log('Isso irá para a minha nova branch. Finja que é uma funcionalidade.')
